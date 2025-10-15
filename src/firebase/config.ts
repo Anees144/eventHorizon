7842570a25be82375e4bd6982bc542998939c9df
multitechs solution <multitechssolution@gmail.com>
2025-10-15 17:58:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7779051371-363a6",
+  "appId": "1:262913328415:web:c27fcb8639a54dcdb81c2a",
+  "apiKey": "AIzaSyD2_Nu8lljIAQq1OZzJT2JYM59zOJ65iOQ",
+  "authDomain": "studio-7779051371-363a6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "262913328415"
+};
